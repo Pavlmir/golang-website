ffd4fff294530b9c97834ec48497c10e75d0b868
Pavel <mirprost@gmail.com>
2022-07-24 13:56:10 +0300

    first commit

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func home_page(page http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(page, "go is super easy!")
+
+}
+
+func main() {
+	http.HandleFunc("/", home_page)
+	http.ListenAndServe(":8080", nil)
+}
